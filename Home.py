f99b7b2012462ffa10d893b8c132a13239fecf9c
iPig <49023948+iPigTW@users.noreply.github.com>
2023-04-01 18:11:14 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,14 @@ import streamlit as st
 st.set_page_config(page_title="iPig's Website", page_icon=":pig:", layout="wide", initial_sidebar_state="expanded")
 st.sidebar.title("iPig's Website")
 st.sidebar.success("Select a page above")
-
+embed = """
+<head>
+<meta content="iPig's Website" property="og:title" />
+<meta content="Python Programmer" property="og:description" />
+<meta content="https://ipigtw.streamlit.app" property="og:url" />
+<meta content="#0096ff" data-react-helmet="true" name="theme-color" />
+</head>
+"""
 with st.container():
     st.title("Welcome to iPig's Website!")
     st.write("This is a website made by iPig using Streamlit.")
